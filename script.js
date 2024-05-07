acd4d228b01bfad0396fa060362249f2a6426245
Felipe Oliveira Souza <153692420+Felipeoliveirasouza@users.noreply.github.com>
2024-05-07 20:17:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const pokemonImage = document.querySelector(".pokemon-image")
+const pokemonName = document.querySelector(".pokemon-name")
+const pokemoNumber = document.querySelector(".pokemon-number")
+
+const form = document.querySelector(".form")
+const input = document.querySelector(".buscar")
+const btnPrev = document.querySelector(".btn-prev")
+const btnNext = document.querySelector(".btn-next")
+
+let pokemonAtual = 1
+
+async function loadPokemon(pokemon) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    const response = await fetch(url)
+    if (response.status === 200) {
+        const data = await response.json()
+        return data
+    }
+    
+}
+ async function renderPokemon(pokemon) {
+    pokemonName.innerHTML = 'Carregando...'
+    pokemoNumber.innerHTML = ''
+
+    const data = await loadPokemon(pokemon)
+    if (data) {
+        pokemonImage.style.display = 'block'
+        pokemonName.innerHTML = data.name
+        pokemoNumber.innerHTML = data.id
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        pokemonAtual = data.id
+        input.value =''
+    }
+    else {
+        pokemonImage.style.display = 'none'
+        pokemonName.innerHTML = 'Não encontrado :c'
+        pokemoNumber.innerHTML = ''
+    }
+
+ }
+
+ // Adicionando evento de envio do formulário
+ form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    renderPokemon(input.value.toLowerCase())
+ })
+
+ btnNext.addEventListener('click', () => {
+    pokemonAtual++
+    renderPokemon(pokemonAtual)
+ })
+
+ btnPrev.addEventListener('click', () => {
+   if ( pokemonAtual > 1) {
+
+       pokemonAtual--
+       renderPokemon(pokemonAtual)
+   }
+  
+ })
+
+
+ renderPokemon(pokemonAtual)
